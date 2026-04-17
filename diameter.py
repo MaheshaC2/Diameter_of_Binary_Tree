fcc6c8524fc6294f1b664379637cb01e233eb630
MaheshaC2 <maheshac6475@gmail.com>
2026-04-17 22:12:03 +0530

    Implement diameter function for binary tree.

---
+++ diameter.py
@@ -0,0 +1,18 @@
+#Diameter of Binary Tree
+def diameter(root):
+    ans = 0
+
+    def height(node):
+        nonlocal ans
+        if not node:
+            return 0
+
+        left = height(node.left)
+        right = height(node.right)
+
+        ans = max(ans, left + right)
+
+        return 1 + max(left, right)
+
+    height(root)
+    return ans
